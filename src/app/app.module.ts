cbec12d5ad27015cdefd54618eac78c6b2b72f66
Diego Gean da Fé <dafediegogean@gmail.com>
2020-09-25 20:27:53 -0300

    Updated app.component.*

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-
 
 import { AppComponent } from './app.component';
 
@@ -16,6 +15,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
 
+    MatIconModule,
     MatInputModule,
     MatButtonModule
   ],
